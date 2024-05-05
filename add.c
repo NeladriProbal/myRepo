f1b162a9f8e603107080bd88bdabad2fedf4d336
NeladriProbal <neeladriprobal45@gmail.com>
2024-05-05 15:41:46 +0600

    wrote_some codes

--- add.c
+++ add.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+fun(int * y)
+{
+    printf("%d",*y);
+    return 0;
+}
 int main()
 {
-    printf("hello Probal\nNow I can easily reach your codes :D\n");
-    printf("Belive me, it's a life-time investment\n");
-    printf("Now change ur github password");
-    //tata
-
-
+    int x=100;
+    int * y=&x;
+    fun(y);
     return 0;
 }
